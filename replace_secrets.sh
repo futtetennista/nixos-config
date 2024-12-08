935bfb86f3f08d0fe07726922196cbd6d5d1426c
Stefano <323267+futtetennista@users.noreply.github.com>
2024-12-08 22:12:28 +0000

    Use secret/config.json

--- replace_secrets.sh
+++ replace_secrets.sh
@@ -3,24 +3,24 @@
 set -euo pipefail
 
 function replace_secrets {
-  if [ ! -f secret-config.json ]; then
-    echo -e "\033[0;31m[replace_secrets.sh] secret-config.json not found!\033[0m"
+  if [ ! -f secret/config.json ]; then
+    echo -e "\033[0;31m[replace_secrets.sh] secret/config.json not found!\033[0m"
     exit 1
   fi
 
   local -a keys=()
   while IFS='' read -r line; do
     keys+=("$line")
-  done < <(jq --raw-output 'keys[]' secret-config.json)
+  done < <(jq --raw-output 'keys[]' secret/config.json)
 
   for key in "${keys[@]}"; do
     if [ "$key" == "@@env@@" ]; then
       continue
     fi
 
-    value=$(jq --raw-output --arg key "$key" '.[$key]' secret-config.json)
+    value=$(jq --raw-output --arg key "$key" '.[$key]' secret/config.json)
     # Find and replace key with value in all files in the repo
-    (grep --recursive --files-with-matches --exclude-dir=secret --exclude=secret-config.json --exclude=replace_secrets.sh "$key" "$PWD" || true) | while read -r file; do
+    (grep --recursive --files-with-matches --exclude-dir=secret --exclude=secret/config.json --exclude=replace_secrets.sh "$key" "$PWD" || true) | while read -r file; do
       sed -i '' "s|$key|$value|g" "$file" || sed -i "s|$key|$value|g" "$file"
     done
   done
@@ -31,12 +31,12 @@ function replace_secrets {
 }
 
 function mk_env_file {
-  env_path=$(jq --raw-output --arg key '@@env.path@@' '.[$key]' secret-config.json)
+  env_path=$(jq --raw-output --arg key '@@env.path@@' '.[$key]' secret/config.json)
   if [ -f "$env_path" ]; then
     rm "$env_path"
   fi
 
-  env=$(jq --raw-output --arg key '@@env@@' '.[$key]' secret-config.json)
+  env=$(jq --raw-output --arg key '@@env@@' '.[$key]' secret/config.json)
   local -a keys=()
   while IFS='' read -r line; do
     keys+=("$line")
