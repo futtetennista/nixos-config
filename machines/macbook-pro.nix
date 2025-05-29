a52c83de4901bb551e3a2163db0df3b1c2e7c58e
Stefano <323267+futtetennista@users.noreply.github.com>
2025-05-29 15:41:01 +0100

    Fix error
    Failed assertions:
           - The option definition `nix.useDaemon' in `/nix/store/r2sdh111qz3q6pjkzmj32fyqgswh5279-source/machines/macbook-pro.nix' no longer has any effect; please remove it.
           nix-darwin now only supports managing multi‐user daemon
           installations of Nix.

--- machines/macbook-pro.nix
+++ machines/macbook-pro.nix
@@ -6,7 +6,7 @@
 
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
-  nix.useDaemon = true;
+  # nix.useDaemon = true;
 
   # Fix to the following error on Intel Macs:
   # > The default Nix build user ID range has been adjusted for
